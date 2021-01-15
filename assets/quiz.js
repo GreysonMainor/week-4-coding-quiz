58d1eea36d914b794a9dfc6851c12005a1daf780
Greyson Mainor <gbmainor98@outlook.com>
2021-01-14 23:32:05 -0500

    added a read me to the code.

--- assets/quiz.js
+++ assets/quiz.js
@@ -2,35 +2,16 @@ var start = document.getElementById("startBtn")
 var openingQuestionsIndex = 0;
 var choices = document.getElementById("choices")
 var currentQuestion = document.getElementById("currentQuestion")
-start.addEventListener("click", function(){
-startQuiz();
-})
+var infoBox = document.getElementById("info-box")
+var infoList = document.getElementById("info-list")
+var infoTitle = document.getElementById("info-title")
+var quizArea = document.getElementById("quiz-area")
+var nextBtn = document.getElementById("next-btn")
+var result = document.getElementById("result")
+var completion = document.getElementById("completion")
+var scoreText = document.getElementById("score-text")
+var restart = document.getElementById("restart")
+var quit = document.getElementById("quit")
+var startQuiz = document.getElementById("start-screen")
 
-function startQuiz(){
-    var startScreen = document.getElementById("start-screen");
-    console.log(start)
-    getQuestion();
-}
 
-function getQuestion(){
-    var openingQuvar start = document.getElementById("startBtn")
-var openingQuestionsIndex = 0;
-var choices = document.getElementById("choices")
-var currentQuestion = document.getElementById("currentQuestion")
-start.addEventListener("click", function(){
-startQuiz();
-})
-
-function startQuiz(){
-    var startScreen = document.getElementById("start-screen");
-    console.log(start)
-    getQuestion();
-}
-
-function getQuestion(){
-    var openingQuestions = questions[openingQuestionsIndex];
-    
-}
-questions = questions[openingQuestionsIndex];
-    
-}
