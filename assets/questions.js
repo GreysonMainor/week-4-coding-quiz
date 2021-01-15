e0b1bcfca429c27fe3343698f02825f75ab293f9
Greyson Mainor <gbmainor98@outlook.com>
2021-01-14 22:53:17 -0500

    added questions to my questions js

---
+++ assets/questions.js
@@ -0,0 +1,39 @@
+var questions = [
+     {
+         question: "What is a binary variable, having two possible values called 'true' and 'false'? ",
+         choiceA: "string",
+         choiceB: "boolean",
+         choiceC: "array",
+         choiceD: "none of these",
+         correctAnswer: "b"
+     },
+     {
+         question: "where are a function's instructions enclosed?",
+         choiceA: "quotes",
+         choiceB: "parentheses",
+         choiceC: "periods",
+         choiceD: "none of these",
+         correctAnswer: "d"
+     },
+     {
+         question: " a function which is used to print any kind of variables defined before in it or to just print any message that needs to be displayed to the user is what?",
+         choiceA: "console.log",
+         choiceB: "javascript",
+         choiceC: "for loop",
+         choiceD: "none of these",
+         correctAnswer: "a"
+     },
+     {
+         question: "What type of pop up would allow a user to fill in a response?",
+         choiceA: "input",
+         choiceB: "prompt",
+         choiceC: "alert",
+         choiceD: "confirm",
+         correctAnswer: "b"
+     },
+    {
+        question: "How many elements can you apply an 'ID' attribute to?",
+        choices: ["As many as you want", "3", "1", "128"],
+        correctAnswer: "1"
+    }
+];
