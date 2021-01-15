7e594eaec9c553052db35844ea5e184d5273ff13
Greyson Mainor <gbmainor98@outlook.com>
2021-01-14 22:56:28 -0500

    made my questions arrays

--- assets/questions.js
+++ assets/questions.js
@@ -1,34 +1,22 @@
 var questions = [
      {
          question: "What is a binary variable, having two possible values called 'true' and 'false'? ",
-         choiceA: "string",
-         choiceB: "boolean",
-         choiceC: "array",
-         choiceD: "none of these",
+         choices:[ "string", "boolean", "array", "none of these"],
          correctAnswer: "b"
      },
      {
          question: "where are a function's instructions enclosed?",
-         choiceA: "quotes",
-         choiceB: "parentheses",
-         choiceC: "periods",
-         choiceD: "none of these",
+         choices:["quotes", "parentheses", "periods", "none of these"],
          correctAnswer: "d"
      },
      {
          question: " a function which is used to print any kind of variables defined before in it or to just print any message that needs to be displayed to the user is what?",
-         choiceA: "console.log",
-         choiceB: "javascript",
-         choiceC: "for loop",
-         choiceD: "none of these",
+        choices:["console.log","javascript", "for loop", "none of these"],
          correctAnswer: "a"
      },
      {
          question: "What type of pop up would allow a user to fill in a response?",
-         choiceA: "input",
-         choiceB: "prompt",
-         choiceC: "alert",
-         choiceD: "confirm",
+         choices: ["input", "prompt", "alert", "confirm"],
          correctAnswer: "b"
      },
     {
