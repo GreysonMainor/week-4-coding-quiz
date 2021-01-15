291a341964ae4cdfc3d70d336e56e538c2d1f956
Greyson Mainor <gbmainor98@outlook.com>
2021-01-15 00:18:32 -0500

    made question page pop up after clicking start

--- assets/questions.js
+++ assets/questions.js
@@ -1,26 +1,26 @@
 var questions = [
      {
-         title: "What is a binary variable, having two possible values called 'true' and 'false'? ",
+         question: "What is a binary variable, having two possible values called 'true' and 'false'? ",
          choices:[ "string", "boolean", "array", "none of these"],
          correctAnswer: "b"
      },
      {
-         title: "where are a function's instructions enclosed?",
+         question: "where are a function's instructions enclosed?",
          choices:["quotes", "parentheses", "periods", "none of these"],
          correctAnswer: "d"
      },
      {
-         title: " a function which is used to print any kind of variables defined before in it or to just print any message that needs to be displayed to the user is what?",
+         question: " a function which is used to print any kind of variables defined before in it or to just print any message that needs to be displayed to the user is what?",
         choices:["console.log","javascript", "for loop", "none of these"],
          correctAnswer: "a"
      },
      {
-         title: "What type of pop up would allow a user to fill in a response?",
+         question: "What type of pop up would allow a user to fill in a response?",
          choices: ["input", "prompt", "alert", "confirm"],
          correctAnswer: "b"
      },
     {
-        title: "How many elements can you apply an 'ID' attribute to?",
+        question: "How many elements can you apply an 'ID' attribute to?",
         choices: ["As many as you want", "3", "1", "128"],
         correctAnswer: "1"
     }
--- assets/quiz.js
+++ assets/quiz.js
@@ -1,7 +1,40 @@
+
+var questions = [
+    {
+        question: "What is a binary variable, having two possible values called 'true' and 'false'? ",
+        choices:[ "string", "boolean", "array", "none of these"],
+        correctAnswer: "b"
+    },
+    {
+        question: "where are a function's instructions enclosed?",
+        choices:["quotes", "parentheses", "periods", "none of these"],
+        correctAnswer: "d"
+    },
+    {
+        question: " a function which is used to print any kind of variables defined before in it or to just print any message that needs to be displayed to the user is what?",
+       choices:["console.log","javascript", "for loop", "none of these"],
+        correctAnswer: "a"
+    },
+    {
+        question: "What type of pop up would allow a user to fill in a response?",
+        choices: ["input", "prompt", "alert", "confirm"],
+        correctAnswer: "b"
+    },
+   {
+       question: "How many elements can you apply an 'ID' attribute to?",
+       choices: ["As many as you want", "3", "1", "128"],
+       correctAnswer: "1"
+   }
+];
+
+
+
+
+
 var startBtn = document.querySelector(".start-btn")
 var openingQuestionsIndex = 0;
 var choices = document.querySelector(".choices")
-var currentQuestion = document.querySelector(".currentQuestion")
+// var currentQuestion = document.querySelector(".currentQuestion")
 var infoBox = document.querySelector(".info-box")
 var infoList = document.querySelector(".info-list")
 var infoTitle = document.querySelector(".info-title")
@@ -13,12 +46,25 @@ var scoreText = document.querySelector(".score-text")
 var restart = document.querySelector(".restart")
 var quit = document.querySelector(".quit")
 var startQuiz = document.querySelector(".start-screen")
-
+// var currentQuestionIndex = 0;
+var questionElement = document.querySelector(".question-text")
 
 function startGame(){
-    console.log("something")
-    startBtn.classList.add("hide")
-    startQuiz.classList.add("active")
+    console.log("something");
+    startBtn.classList.add("hide");
+    infoBox.classList.add("hide");
+    startQuiz.classList.add("active");
+    setNextQuestion();
+}
+
+function setNextQuestion(){
+    showQuestion([openingQuestionsIndex])
+
+}
+
+function showQuestion(){
+questionElement.innerHTML = (openingQuestionsIndex)
 }
 
 startBtn.addEventListener("click", startGame)
+
