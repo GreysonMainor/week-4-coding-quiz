f18d10dba395b9c9c266ffb2d96aaa13abdf00c6
Greyson Mainor <gbmainor98@outlook.com>
2021-01-14 23:05:44 -0500

    addedvariables and attempting to get my javascript to do something in my quiz.js folder.

---
+++ assets/quiz.js
@@ -0,0 +1,36 @@
+var start = document.getElementById("startBtn")
+var openingQuestionsIndex = 0;
+var choices = document.getElementById("choices")
+var currentQuestion = document.getElementById("currentQuestion")
+start.addEventListener("click", function(){
+startQuiz();
+})
+
+function startQuiz(){
+    var startScreen = document.getElementById("start-screen");
+    console.log(start)
+    getQuestion();
+}
+
+function getQuestion(){
+    var openingQuvar start = document.getElementById("startBtn")
+var openingQuestionsIndex = 0;
+var choices = document.getElementById("choices")
+var currentQuestion = document.getElementById("currentQuestion")
+start.addEventListener("click", function(){
+startQuiz();
+})
+
+function startQuiz(){
+    var startScreen = document.getElementById("start-screen");
+    console.log(start)
+    getQuestion();
+}
+
+function getQuestion(){
+    var openingQuestions = questions[openingQuestionsIndex];
+    
+}
+questions = questions[openingQuestionsIndex];
+    
+}
