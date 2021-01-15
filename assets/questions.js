bf00dd7f01eab57a9e4fcc5ed5125009968f4942
Greyson Mainor <gbmainor98@outlook.com>
2021-01-14 23:15:15 -0500

    updated a little css to hide certain items.  Added buttons and divs for answers once i figure out how to put them in.

--- assets/questions.js
+++ assets/questions.js
@@ -1,26 +1,26 @@
 var questions = [
      {
-         question: "What is a binary variable, having two possible values called 'true' and 'false'? ",
+         title: "What is a binary variable, having two possible values called 'true' and 'false'? ",
          choices:[ "string", "boolean", "array", "none of these"],
          correctAnswer: "b"
      },
      {
-         question: "where are a function's instructions enclosed?",
+         title: "where are a function's instructions enclosed?",
          choices:["quotes", "parentheses", "periods", "none of these"],
          correctAnswer: "d"
      },
      {
-         question: " a function which is used to print any kind of variables defined before in it or to just print any message that needs to be displayed to the user is what?",
+         title: " a function which is used to print any kind of variables defined before in it or to just print any message that needs to be displayed to the user is what?",
         choices:["console.log","javascript", "for loop", "none of these"],
          correctAnswer: "a"
      },
      {
-         question: "What type of pop up would allow a user to fill in a response?",
+         title: "What type of pop up would allow a user to fill in a response?",
          choices: ["input", "prompt", "alert", "confirm"],
          correctAnswer: "b"
      },
     {
-        question: "How many elements can you apply an 'ID' attribute to?",
+        title: "How many elements can you apply an 'ID' attribute to?",
         choices: ["As many as you want", "3", "1", "128"],
         correctAnswer: "1"
     }
